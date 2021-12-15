33d8a086861d25611664ce136e385ce5a3057f0e
nbhanuprasad <nbhanuprasad@gmail.com>
2021-12-15 09:46:09 -0500

    main

--- timelogapp/apps.py
+++ timelogapp/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
+import os
+from django.conf import settings
 
 
 class TimelogappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'timelogapp'
+    path = './timelogapp'
